6365609610ca74a8eb3d53845a5f449831e4038e
Alfred Landrum <alfred@leakybucket.org>
2023-06-23 13:11:41 -0700

    draft: shard linger feature

--- service/history/shard/controller_impl.go
+++ service/history/shard/controller_impl.go
@@ -34,6 +34,7 @@ import (
 
 	"go.opentelemetry.io/otel/trace"
 	"go.temporal.io/api/serviceerror"
+	"golang.org/x/time/rate"
 
 	"go.temporal.io/server/api/historyservice/v1"
 	"go.temporal.io/server/client"
@@ -229,7 +230,7 @@ func (c *ControllerImpl) ShardIDs() []int32 {
 	return ids
 }
 
-func (c *ControllerImpl) shardClosedCallback(shard *ContextImpl) {
+func (c *ControllerImpl) shardRemoveAndStop(shard *ContextImpl) {
 	startTime := time.Now().UTC()
 	defer func() {
 		c.taggedMetricsHandler.Timer(metrics.RemoveEngineForShardLatency.GetMetricName()).Record(time.Since(startTime))
@@ -294,7 +295,7 @@ func (c *ControllerImpl) getOrCreateShardContext(shardID int32) (*ContextImpl, e
 		shardID,
 		c.engineFactory,
 		c.config,
-		c.shardClosedCallback,
+		c.shardRemoveAndStop,
 		c.logger,
 		c.throttledLogger,
 		c.persistenceExecutionManager,
@@ -375,6 +376,37 @@ func (c *ControllerImpl) shardManagementPump() {
 	}
 }
 
+func (c *ControllerImpl) shardLingerThenClose(shardID int32) {
+	c.RLock()
+	shard, ok := c.historyShards[shardID]
+	c.RUnlock()
+	if !ok || !shard.IsValid() {
+		return
+	}
+
+	// Delay closing the shard for a small amount of time, and see if the
+	// shard becomes invalid due to a request to the shard receiving a
+	// shard ownership lost error.
+	timeout := util.Min(c.config.ShardLingerTimeLimit(), shardIOTimeout)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	qps := c.config.ShardLingerOwnershipCheckQPS()
+	limiter := rate.NewLimiter(rate.Limit(qps), qps)
+
+	for {
+		_ = shard.AssertOwnership(ctx)
+		if !shard.IsValid() {
+			return
+		}
+
+		if err := limiter.Wait(ctx); err != nil {
+			c.shardRemoveAndStop(shard)
+			return
+		}
+	}
+}
+
 func (c *ControllerImpl) acquireShards() {
 	c.taggedMetricsHandler.Counter(metrics.AcquireShardsCounter.GetMetricName()).Record(1)
 	startTime := time.Now().UTC()
@@ -390,7 +422,11 @@ func (c *ControllerImpl) acquireShards() {
 		}
 		if info.Identity() != c.hostInfoProvider.HostInfo().Identity() {
 			// current host is not owner of shard, unload it if it is already loaded.
-			c.CloseShardByID(shardID)
+			if c.config.ShardLingerEnabled() {
+				c.shardLingerThenClose(shardID)
+			} else {
+				c.CloseShardByID(shardID)
+			}
 			return
 		}
 		shard, err := c.GetShardByID(shardID)
